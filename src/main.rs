5f52919044cafd10ef50ab4b5be82b2395a0d9f2
Will Harris <will.t.harris@protonmail.com>
2020-06-19 20:48:10 -0700

    grab pattern and path from cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
 }
